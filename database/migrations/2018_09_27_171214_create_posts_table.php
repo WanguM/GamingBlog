e95192ecb35c2e7fa5f403365fe6a02a7a3f2309
bruce <bruce00406@gmail.com>
2018-09-28 23:38:49 +0800

    able to create categories

--- database/migrations/2018_09_27_171214_create_posts_table.php
+++ database/migrations/2018_09_27_171214_create_posts_table.php
@@ -20,6 +20,9 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->text('body');
             $table->timestamps();
+            // user_id 參考來自users 資料表的id
+            // CASCADE  階層狀異動：主表刪子表就刪；主表修子表就修。
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
